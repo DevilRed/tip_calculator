2ebf4f341effb98fb32fe9058855bb1d94430429
Christian <chrisdaq@gmail.com>
2025-04-11 16:37:24 -0400

    show order items on consumo side

--- src/components/OrderContent.tsx
+++ src/components/OrderContent.tsx
@@ -9,15 +9,27 @@ export const OrderContent = ({ order }: OrderContentProps) => {
   return (
     <div>
       <h2 className="font-black text-4xl">Consumo</h2>
-      <div className="space-y-3 mt-5">
+      <div className="space-y-3 mt-10">
         {order.length === 0 ? (
           <p className="text-center">Order is empty</p>
         ) : (
           order.map((item) => (
-            <div key={item.id}>
-              <p>
-                {item.name} - {formatCurrency(item.price)}
-              </p>
+            <div
+              key={item.id}
+              className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type: border-b"
+            >
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className="font-black">
+                  Quantity: {item.quantity} -
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+              <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black">
+                X
+              </button>
             </div>
           ))
         )}
