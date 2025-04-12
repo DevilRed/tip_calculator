28b883da9670e223b90f5374701d8578c4d1ba26
Christian <chrisdaq@gmail.com>
2025-04-12 10:46:34 -0400

    add tip percentage form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { MenuItem } from "./components/MenuItem";
 import { OrderContent } from "./components/OrderContent";
 import { OrderTotals } from "./components/OrderTotals";
+import { TipPercentageForm } from "./components/TipPercentageForm";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
@@ -23,6 +24,7 @@ function App() {
         </div>
         <div className="border border-dashed border-slate-300 py-5 rounded-lg space-y-10">
           <OrderContent order={order} removeItem={removeItem} />
+          <TipPercentageForm />
           <OrderTotals order={order} />
         </div>
       </main>
