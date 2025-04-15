505116ce60d755d8f0d5a19fc17ebb39f8bca67c
Christian <chrisdaq@gmail.com>
2025-04-15 15:40:12 -0400

    config vitest and update typescript config

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+ plugins: [react()],
+ test: {
+   environment: 'jsdom',
+   globals: true,
+   setupFiles: ['./tests/setupTest'],
+   coverage: {
+     reporter: ['text', 'json', 'html'],
+   },
+ },
+ resolve: {
+   alias: {
+     '@': path.resolve(__dirname, './src'),
+     // '@features': path.resolve(__dirname, './src/redux/features'),
+     // '@hooks': path.resolve(__dirname, './src/hooks'),
+     // '@store': path.resolve(__dirname, './src/redux/store'),
+   },
+ },
+});
